c8cb8a758cfb04d3046d0354cff7b336284cafda
rosario <andolinarosario@gmail.com>
2023-03-23 02:44:04 +0100

    shared queue not working yet

--- Map2H5/DynamicReader.py
+++ Map2H5/DynamicReader.py
@@ -1,4 +1,5 @@
-import threading
+import multiprocessing
+from queue import Queue
 import io
 import re
 from glob import glob
@@ -11,18 +12,18 @@ from Map2H5.FastFit import FastFit
 from XRDXRFutils import DataXRF
 
 class DynamicReader():
-    def __init__(self, master, path, ndetector = 2):
-        self.master = master
+    def __init__(self, root, path, ndetector = 2):
+        self.root = root
         self.path = path
         self.ndetector = ndetector
         head, tail = pathsplit(self.path)
         self.dpath = [join(self.path, tail + f'_{i}') for i in range(1, self.ndetector + 1)]
-        self.queue = [list() for i in range(self.ndetector)]
-        self.queuelock = [threading.Lock() for i in range(self.ndetector)]
-        self.fitlock = threading.Lock()
+        self.queue = [Queue() for i in range(self.ndetector)]
+        self.queuelock = [multiprocessing.Lock() for i in range(self.ndetector)]
+        self.fitlock = multiprocessing.Lock()
         self.get_scanning_par()
-        self.observer = [Observer(f'observer{i+1}', self.dpath[i], self.queue[i], self.queuelock[i]) for i in range(self.ndetector)]
-        self.reader = [Reader(f'reader{i+1}',self.master, self.queue[i], self.queuelock[i], self.fitlock, self.rowlen, self.nrow) for i in range(self.ndetector)]
+        self.observer = [Observer(self, i+1 ,f'observer{i+1}', self.dpath[i]) for i in range(self.ndetector)]
+        self.reader = [Reader(self.root, self, i+1, f'reader{i+1}', self.rowlen, self.nrow) for i in range(self.ndetector)]
         #print(f"dynamicReader: {len(self.observer)} observers {len(self.reader)} readers {self.ndetector} detectors")
     
     def get_scanning_par(self):
@@ -52,16 +53,17 @@ class DynamicReader():
             self.reader[i].join()
         
         
-class Observer(threading.Thread):
-    def __init__(self, name, path, queue, lock):
+class Observer(multiprocessing.Process):
+    def __init__(self, master, threadId,  name, path):
         super().__init__()
+        self.master = master
+        self.threadId = threadId
         self.name = name
         self.path = path
-        self.queue = queue
-        self.lock = lock
+        self.queue = self.master.queue[self.threadId - 1]
+        self.lock = self.master.queuelock[self.threadId - 1]
         self.filelist = []
         self.whatch = True
-        #print(f"{self.name} observing")
     
     def run(self):
         while self.whatch:
@@ -69,19 +71,24 @@ class Observer(threading.Thread):
             newfiles = [x for x in newfilelist if x not in self.filelist]
             if len(newfiles) != 0:
                 self.lock.acquire()
-                self.queue += newfiles
-                self.lock.release()
+                try:
+                    for nf in newfiles:
+                        self.queue.put(nf)
+                finally:
+                    self.lock.release()
                 self.filelist = newfilelist
             sleep(0.1)
 
-class Reader(threading.Thread):
-    def __init__(self, name, master, queue, queuelock, fitlock, *scanning_par):
+class Reader(multiprocessing.Process):
+    def __init__(self, root, master, threadId, name, *scanning_par):
         super().__init__()
-        self.name = name
+        self.root = root
         self.master = master
-        self.queue = queue
-        self.queuelock = queuelock
-        self.fitlock = fitlock
+        self.threadId = threadId
+        self.name = name
+        self.queue = self.master.queue[self.threadId - 1]
+        self.queuelock = self.master.queuelock[self.threadId - 1]
+        self.fitlock = self.master.fitlock
         self.read = True
         self.ftoread = None
         self.rowlen, self.nrow = scanning_par
@@ -90,11 +97,15 @@ class Reader(threading.Thread):
 
         
     def run(self):
+        print(f'{self.name} queue lenght {self.queue.qsize()}')
         while self.read:
             self.queuelock.acquire()
-            if len(self.queue) >= 2:
-                self.ftoread = self.queue.pop(0)
-            self.queuelock.release()
+            try:
+                if self.queue.qsize() >= 2:
+                    self.ftoread = self.queue.get()
+                    print(f'{self.name} reading queuelen {self.queue.qsize()}')
+            finally:
+                self.queuelock.release()
             if self.ftoread: self.process_file()
             self.ftoread = None
             sleep(0.1)
@@ -142,18 +153,28 @@ class Reader(threading.Thread):
         if n%2 == 0:
             x = flip(x)
         self.fitlock.acquire()
-        ffit = FastFit(data = x, cfgfile = self.master.cfgfile, outputdir = self.master.outputdir)
-        if exists(ffit.filename): remove(ffit.filename)
-        ffit.fit()
-        #l = ffit.get_labels()
-        remove(ffit.filename)
-        for i,(k,v) in enumerate(ffit.get_labels().items()):
-            self.master.labels[k][n] += v
-        self.master.data.data[n] += x
-        self.master.update_imarray()
-        self.fitlock.release()
+        try:
+            ffit = FastFit(data = x, cfgfile = self.master.cfgfile, outputdir = self.master.outputdir)
+            if exists(ffit.filename): remove(ffit.filename)
+            ffit.fit()
+            #l = ffit.get_labels()
+            remove(ffit.filename)
+            for i,(k,v) in enumerate(ffit.get_labels().items()):
+                self.root.labels[k][n] += v
+            self.root.data.data[n] += x
+            self.root.update_imarray()
+        finally:
+            self.fitlock.release()
 
     def finalize(self):
-        while self.queue:
-            self.process_file(_file = self.queue.pop(0))
+        while True:
+            self.queuelock.aquire()
+            try:
+                if not self.queue.empty():
+                    self.process_file(_file = self.queue.get())
+                else:
+                    break
+            finally:
+                self.queuelock.release()
+                
         
